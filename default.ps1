7843241c4db7e18f0e2f412b43c94f77e17b7d55
$+j <lust4life.jun@gmail.com>
2015-11-16 11:53:10 +0800

    use uoko nuget push

--- default.ps1
+++ default.ps1
@@ -92,7 +92,7 @@ task CreateNuGetPackage -depends ILMerge {
 	}
 
 
-	copy-item $src_directory\IdentityServer3.nuspec $dist_directory
+	copy-item $src_directory\IdentityServer3-uoko.nuspec $dist_directory
 	copy-item $output_directory\IdentityServer3.xml $dist_directory\lib\net45\
-	exec { . $nuget_path pack $dist_directory\IdentityServer3.nuspec -BasePath $dist_directory -o $dist_directory -version $packageVersion }
+	exec { . $nuget_path pack $dist_directory\IdentityServer3-uoko.nuspec -BasePath $dist_directory -o $dist_directory -version $packageVersion }
 }
